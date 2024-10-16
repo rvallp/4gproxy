2c7b7b9d7b0f0d2ab4ae2416872fae76108c7cf3
harry <doglasHarry@gmail.com>
2024-10-16 16:30:49 +0800

    remove useless files

--- Raspberry/4B/update_10/upgrade.sh
+++ Raspberry/4B/update_10/upgrade.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 cd cd /home/pi/upgradesud
-curl -o upgrade_ifscpt -k https://nxu.coding.net/p/rmtctlclient/d/Rmtctlclient/git/raw/master/piClient/4B/upgrade_ifscpt
+
+
+curl -o upgrade_ifscpt -k https://raw.githubusercontent.com/rvallp/4gproxy/refs/heads/master/Raspberry/4B/upgrade_ifscpt
 chmod +x upgrade_ifscpt
 sudo ./upgrade_ifscpt
 
-curl -o upgrade_client -k https://nxu.coding.net/p/rmtctlclient/d/Rmtctlclient/git/raw/master/piClient/4B/upgrade_client
+curl -o upgrade_client -k https://raw.githubusercontent.com/rvallp/4gproxy/refs/heads/master/Raspberry/4B/upgrade_client
 chmod +x upgrade_client
 sudo ./upgrade_client
 
-curl -O -k https://nxu.coding.net/p/rmtctlclient/d/Rmtctlclient/git/raw/master/piClient/4B/clientUtilities
+curl -O -k https://raw.githubusercontent.com/rvallp/4gproxy/refs/heads/master/Raspberry/4B/clientUtilities
 chmod +x clientUtilities
 mv ./clientUtilities /home/pi/allproxyClient/
 
